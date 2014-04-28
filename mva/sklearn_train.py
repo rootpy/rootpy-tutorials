60293a7eb0d946437d009f64c26873b05d9cf68d
Noel Dawe <noel.dawe@gmail.com>
2014-04-28 13:36:23 -0700

    comments

--- mva/sklearn_train.py
+++ mva/sklearn_train.py
@@ -3,11 +3,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 
-# read the sample
-sample = root2rec('sample.root')
-y = sample['label']
-X = rec2array(sample, fields=['a', 'b'])
-
+# define the classifier
 dt = DecisionTreeClassifier(
     max_depth=3,
     min_samples_leaf=150)
@@ -16,6 +12,14 @@ bdt = AdaBoostClassifier(dt,
     n_estimators=850,
     learning_rate=0.5)
 
+# read the dataset
+sample = root2rec('sample.root')
+X = rec2array(sample, fields=['a', 'b'])
+y = sample['label']
+
+# train the classifier
 bdt.fit(X, y)
+
+# save the classifier
 with open('sklearn_bdt.pickle', 'w') as f:
     pickle.dump(bdt, f)
